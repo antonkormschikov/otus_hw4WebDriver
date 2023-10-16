638ee89ddea512f14f64cd11b0d8575f17f8c2aa
antonkormschikov <antonkormschikov@yandex.ru>
2023-10-16 22:46:25 +0300

    locator update

--- src/test/java/WebDriverTests.java
+++ src/test/java/WebDriverTests.java
@@ -10,8 +10,7 @@ import java.util.NoSuchElementException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
 
 
 public class WebDriverTests {
@@ -21,18 +20,7 @@ public class WebDriverTests {
     @BeforeAll
     public static void setup() {
 
-        //WebDriverManager.chromedriver().setup();
-        WebDriverManager.firefoxdriver().setup();
-    }
-
-    @Disabled
-    @BeforeEach
-    public void start() {
-
-
-       /* FirefoxOptions options = new FirefoxOptions();
-        options.setHeadless(true);
-        driver=new FirefoxDriver(options);*/
+           WebDriverManager.firefoxdriver().setup();
     }
 
     @AfterEach
@@ -42,8 +30,7 @@ public class WebDriverTests {
         }
     }
 
-
-    @Test
+     @Test
     public void headlessTest() throws NoSuchElementException  {
         //Инициализация драйвера вынесена в тест т.к., в разных тестах браузер запускается в разных режимах
         //ChromeOptions options = new ChromeOptions();
@@ -74,14 +61,12 @@ public class WebDriverTests {
         driver=new FirefoxDriver(options);
         driver.get("https://demo.w3layouts.com/demos_new/template_demo/03-10-2020/photoflash-liberty-demo_Free/685659620/web/index.html?_ga=2.181802926.889871791.1632394818-2083132868.1632394818");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.findElement(new By.ByXPath("//li[@data-id='id-2' and @class='portfolio-item2 content']")).click();
+        driver.findElement(new By.ByXPath("//li/span/a[@href='assets/images/p3.jpg']")).click();
         WebElement modalWindow = driver.findElement(new By.ByXPath("//div[@class='pp_content_container']"));
 
         Assertions.assertEquals(true,modalWindow.isDisplayed());
 
     }
-
-
 
     @Test
     public void cookesTest() throws NoSuchElementException{
@@ -95,7 +80,7 @@ public class WebDriverTests {
         loginInput.clear();loginInput.sendKeys("oxilqrxobfqlrd@hldrive.com");
         WebElement passInput = driver.findElement(new By.ByXPath("//input[@type='password']"));
         passInput.clear(); passInput.sendKeys("Opera-324");
-        driver.findElement(new By.ByXPath("//button[@class='sc-9a4spb-0 gYNtqF sc-11ptd2v-2-Component cElCrZ']")).click();
+        driver.findElement(new By.ByXPath("//button/div[text()='Войти']")).click();
         logger.info(String.format("Выводим в логи Cookies с otus.ru %s", driver.manage().getCookies().toString()));
     }
 
